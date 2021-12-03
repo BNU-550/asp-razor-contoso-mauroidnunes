5d532cb7f8e8c23cee6660ecf8055fad914ec12e
Mauro Nunes <21815118@bucks.ac.uk>
2021-12-03 12:17:02 +0000

    mid

--- ASP_Razor_Contoso/ASP_Razor_Contoso/Pages/Index.cshtml.cs
+++ ASP_Razor_Contoso/ASP_Razor_Contoso/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ASP_Razor_Contoso.Pages
+namespace ASP_RazorContoso.Pages
 {
     public class IndexModel : PageModel
     {
--- ASP_Razor_Contoso/ASP_Razor_Contoso/Pages/Students/Index.cshtml.cs
+++ ASP_Razor_Contoso/ASP_Razor_Contoso/Pages/Students/Index.cshtml.cs
@@ -1,29 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ASP_Razor_Contoso.Data;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ASP_Razor_Contoso.Data;
-using ContosoUniversity.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ASP_Razor_Contoso.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly ASP_Razor_Contoso.Data.ApplicationDbContext _context;
-
-        public IndexModel(ASP_Razor_Contoso.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.ToListAsync();
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
+
